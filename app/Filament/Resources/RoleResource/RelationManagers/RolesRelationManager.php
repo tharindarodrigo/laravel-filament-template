d059e3373eae669dee1654c7a3edf9332405c93f
Tharinda Rodrigo <tharinda.rodrigo@pictureworks.com.au>
2022-04-08 22:05:36 +0530

    User Resource modifications

---
+++ app/Filament/Resources/RoleResource/RelationManagers/RolesRelationManager.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Filament\Resources\RoleResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\RelationManagers\BelongsToManyRelationManager;
+use Filament\Resources\Table;
+use Filament\Tables;
+
+class RolesRelationManager extends BelongsToManyRelationManager
+{
+    protected static string $relationship = 'roles';
+
+    protected static ?string $recordTitleAttribute = 'name';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+            ])
+            ->filters([
+                //
+            ]);
+    }
+}
--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -2,11 +2,13 @@
 
 namespace App\Filament\Resources;
 
+use App\Filament\Resources\RoleResource\RelationManagers\RolesRelationManager;
 use App\Filament\Resources\UserResource\Pages;
-use App\Filament\Resources\UserResource\RelationManagers;
 use App\Models\User;
 use Filament\Forms;
-use Filament\Forms\Components\Component;
+use Filament\Forms\Components\Card;
+use Filament\Forms\Components\Fieldset;
+use Filament\Forms\Components\Grid;
 use Filament\Resources\Form;
 use Filament\Resources\Resource;
 use Filament\Resources\Table;
@@ -21,12 +23,19 @@ class UserResource extends Resource
     public static function form(Form $form): Form
     {
         return $form
-        ->schema([
-            Forms\Components\TextInput::make('name')->required(),
-            Forms\Components\TextInput::make('email')->email()->required(),
-            Forms\Components\TextInput::make('password')->password()->required(),
-            Forms\Components\TextInput::make('password_again')->password()->same('password')
-        ]);
+            ->schema([
+                Card::make()
+                    ->schema([
+                        Grid::make(2)
+                            ->schema([
+                                Forms\Components\TextInput::make('name')->required(),
+                                Forms\Components\TextInput::make('email')->email()->required(),
+                                Forms\Components\TextInput::make('password')->password()->required(),
+                                Forms\Components\TextInput::make('password again')->password()->same('password'),
+                                Forms\Components\BelongsToManyCheckboxList::make('roles')->relationship('roles', 'name')
+                            ])
+                    ])
+            ]);
     }
 
     public static function table(Table $table): Table
@@ -44,7 +53,7 @@ class UserResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            RolesRelationManager::class
         ];
     }
 
@@ -53,7 +62,21 @@ class UserResource extends Resource
         return [
             'index' => Pages\ListUsers::route('/'),
             'create' => Pages\CreateUser::route('/create'),
+            'view' => Pages\ViewUser::route('/{record}'),
             'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getFormSchema(): array
+    {
+        return [
+            Fieldset::make('Label')->schema([
+                Forms\Components\TextInput::make('name')->required(),
+                Forms\Components\TextInput::make('email')->email()->required(),
+                Forms\Components\TextInput::make('password')->password()->required(),
+                Forms\Components\TextInput::make('password again')->password()->same('password'),
+                Forms\Components\BelongsToManyCheckboxList::make('roles')->relationship('roles', 'name')
+            ])
         ];
     }
 }
